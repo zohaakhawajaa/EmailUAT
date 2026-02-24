ab6ea8c9c6b34b49e17d73c0331550e590a26c28
Zoha Khawaja <zohaakhawajaa@gmail.com>
2026-02-24 11:44:28 +0500

    Your commit message

---
+++ ClickChain/src/main/java/EmailNotificationService.java
@@ -0,0 +1,6 @@
+public class EmailNotificationService {
+    public boolean isValidRecipient(String email) {
+        if (email == null) return false;
+        return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    }
+}
